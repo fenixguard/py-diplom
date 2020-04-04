412a0e1bab14123a81a341ad3c8468910684467e
Sergey Volkov <fenixguard1993@gmail.com>
2020-04-04 12:25:24 +0300

    fix bufs, and start create method get_groups

--- vk_api.py
+++ vk_api.py
@@ -3,15 +3,30 @@ import requests
 VK_URL = 'https://api.vk.com/method/'
 VK_TOKEN = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
 
-METHOD_NAME = "friends.get"
-PARAMETERS = "user_id=25973125"
-ACCESS_TOKEN = f"access_token={VK_TOKEN}"
-V = f"v={5.103}"
 
-payload = f"{PARAMETERS}&{ACCESS_TOKEN}&{V}"
-VK_URL = f"{VK_URL}{METHOD_NAME}"
+def get_friends() -> list:
+    method_name = "friends.get"
+    parameters = "user_id=171691064"
+    access_token = f"access_token={VK_TOKEN}"
+    version_api = f"v={5.103}"
+
+    payload = f"{parameters}&{access_token}&{version_api}"
+    vk_url = f"{VK_URL}{method_name}"
+
+    response = requests.get(vk_url, params=payload)
+    json_dict = response.json()
+    return json_dict['response']['items']
 
 
-response = requests.get(VK_URL, params=payload)
-json_dict = response.json()
-print(response)
+def get_groups(friend) -> list:
+    
+
+
+    return []
+
+
+if __name__ == '__main__':
+    friends = get_friends()
+    for friend in friends:
+        get_groups(friend)
+    print(friends)
